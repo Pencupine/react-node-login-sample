3abaa93f94fb5085bc999bcc9d53cdf2abd69803
pencupine527 <pencupine527.c@gmail.com>
2018-06-13 12:38:45 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const idKeys = require('./reqkeys/keys');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/keys', (req, res) => {
+
+  res.json(idKeys);
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`App listening on ${port}`);
