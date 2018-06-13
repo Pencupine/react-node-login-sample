a9a0d51ffef32b1079eae31218766c89e6395fcd
pencupine527 <pencupine527.c@gmail.com>
2018-06-13 12:49:35 +0530

    removed / in stattic fucntion

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const idKeys = require('./reqkeys/keys');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/api/keys', (req, res) => {
 
   res.json(idKeys);
 });
+
+
 
 const port = process.env.PORT || 5000;
 app.listen(port);
